4c8b40b63f32b6d002363332c83dcaff4972b4f7
monarosa22 <monarosanah66@gmail.com>
2023-07-03 20:55:36 +0700

    Create Migration tabel laporan kegiatan

---
+++ database/migrations/2023_07_03_133955_create_laporan_kegiatan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('laporan_kegiatan', function (Blueprint $table) {
+            $table->string("id", 50)->primary();
+            $table->string("user_id", 50);
+            $table->string("izin_kegiatan_id", 50);
+            $table->string("file_lpj");
+            $table->string("foto_dokumentasi");
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laporan_kegiatan');
+    }
+};
